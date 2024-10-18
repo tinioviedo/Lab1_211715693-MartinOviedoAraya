638e516d136a72899d4264b6533a89469b00bd64
tinioviedo <martin.oviedo@usach.cl>
2024-10-18 20:29:54 -0300

    Inicio de implementación del TDA board

---
+++ TDAboard_211715693_MartinOviedo.rkt
@@ -0,0 +1,41 @@
+#lang racket
+
+;Descripcion: Crear una lista para representar el tablero
+;Dominio: no hay parametros de entrada
+;Recorrido : board (un tablero vacio)
+;Constructor
+
+(define board
+  '((0 0 0 0 0 0 0)
+    (0 0 0 0 0 0 0)
+    (0 0 0 0 0 0 0)
+    (0 0 0 0 0 0 0)
+    (0 0 0 0 0 0 0)
+    (0 0 0 0 0 0 0)))
+
+
+
+
+
+
+
+
+
+
+       
+       
+       
+     
+
+
+
+
+
+
+
+
+
+ 
+  
+
+
